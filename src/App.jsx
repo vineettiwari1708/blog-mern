b5f1fbdc5b2834dfd09635a0638e417761432cbd
vineettiwari1708 <vineettiwari1708@gmail.com>
2026-02-04 17:54:14 +0530

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,50 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Blog from './pages/Blog'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Blog from './pages/Blog';
+import Dashboard from './pages/admin/Dashboard';
+import AddBlog from './pages/admin/AddBlog';
+import ListBlog from './pages/admin/ListBlog';
+import Comment from './pages/admin/Comment';
+import Layout from './pages/admin/Layout';
+import Login from './components/admin/Login';
 
 const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/blog/:id' element={<Blog/>}/>
-     </Routes>
-    </div>
-  )
-}
+	return (
+		<div>
+			<Routes>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
+				<Route
+					path="/blog/:id"
+					element={<Blog />}
+				/>
+				<Route
+					path="/admin"
+					element={true ? <Layout />: <Login/>}
+				>
+					<Route
+						index
+						element={<Dashboard />}
+					/>
+					<Route
+						path="addBlog"
+						element={<AddBlog />}
+					/>
+					<Route
+						path="listBlog"
+						element={<ListBlog/>}
+					/>
+          <Route
+						path="comments"
+						element={<Comment/>}
+					/>
+				</Route>
+			</Routes>
+		</div>
+	);
+};
 
-export default App
+export default App;
