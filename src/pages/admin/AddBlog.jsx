c4432977b202bd4f82a8540da9f09b53af54b055
vineettiwari1708 <vineettiwari1708@gmail.com>
2026-02-06 14:54:09 +0530

    addblog

--- src/pages/admin/AddBlog.jsx
+++ src/pages/admin/AddBlog.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { assets } from '../../assets/assets';
+import { assets, blogCategories } from '../../assets/assets';
 import Quill from 'quill';
 
 const AddBlog = () => {
-	const editorRef = useRef(null)
-	const quillRef = useRef(null)
+	const editorRef = useRef(null);
+	const quillRef = useRef(null);
 	const [image, setImage] = useState(false);
 	const [title, setTitle] = useState('');
 	const [subTitle, setSubTitle] = useState('');
@@ -14,11 +14,11 @@ const AddBlog = () => {
 	const onSubmitHandler = async (e) => {
 		e.preventDefault();
 	};
-	useEffect(()=>{
-		if(!quillRef.current && editorRef.current){
-			quillRef.current = new Quill(editorRef.current, {theme:'snow'})
+	useEffect(() => {
+		if (!quillRef.current && editorRef.current) {
+			quillRef.current = new Quill(editorRef.current, { theme: 'snow' });
 		}
-	},[])
+	}, []);
 	return (
 		<form
 			onSubmit={onSubmitHandler}
@@ -69,6 +69,34 @@ const AddBlog = () => {
 						Generate with AI
 					</button>
 				</div>
+				<p className="mt-4">Blog Category</p>
+				<select
+					onChange={(e) => setCategory(e.target.value)}
+					name="category"
+					className="mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded"
+				>
+					<option value="">Select Category</option>
+					{blogCategories.map((item, index) => {
+						return (
+							<option
+								key={index}
+								value={item}
+							>
+								{item}
+							</option>
+						);
+					})}
+				</select>
+				<div className='flex gap-2 mt-4'>
+					<p>Publish Now</p>
+					<input
+						type="checkbox"
+						checked={isPublished}
+						className="scale-125 cursor-pointer"
+						onChange={(e) => setIsPublished(e.target.checked)}
+					/>
+				</div>
+				<button type="submit" className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'>Add Blog</button>
 			</div>
 		</form>
 	);
